02aa1bf5a729937c436e854b34b41e9742645416
him16112 <99254194+him16112@users.noreply.github.com>
2024-05-23 09:13:03 +0530

    Update Registration.js

--- src/frontend/Registration.js
+++ src/frontend/Registration.js
@@ -71,7 +71,7 @@ const Registration = () => {
     }
 
     try {
-      const response = await fetch(`https://w3villa-k6hl.onrender.com/register`, {
+      const response = await fetch('http://localhost:8000/register', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
